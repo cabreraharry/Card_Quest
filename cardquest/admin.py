51117662af3a600fd6cb51e893a1171ab3b65580
Harry Cabrera <harryericsoncabrera15@gmail.com>
2023-12-06 11:04:34 +0800

    admin, settings and gitignore updated

--- cardquest/admin.py
+++ cardquest/admin.py
@@ -4,7 +4,7 @@ from .models import PokemonCard
 
 admin.site.register(PokemonCard)
 
-@admin.register(PokemonCard)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ("name", "rarity")
-    search_fields = ("name",)
+# @admin.register(PokemonCard)
+# class PokemonAdmin(admin.ModelAdmin):
+#     list_display = ("name", "rarity")
+#     search_fields = ("name",)
