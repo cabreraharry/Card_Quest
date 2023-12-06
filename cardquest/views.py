31fe6ecfe147cc28ae167cb7eed29d09b57b3c6e
Harry Cabrera <harryericsoncabrera15@gmail.com>
2023-12-06 14:35:43 +0800

    Django Pics Materials & instructions Completed

--- cardquest/views.py
+++ cardquest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+from cardquest.models import PokemonCard, Trainer
+
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15
